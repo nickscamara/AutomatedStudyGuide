853e27e66d844b1f5516ebcd65ac36547d5281d2
Nicolas <nicolascamara29@gmail.com>
2019-02-28 15:26:40 -0500

    mid day commit

--- docreader.py
+++ docreader.py
@@ -1,4 +1,18 @@
 import docx2txt
+import re
 
-my_document = docx2txt.process("static/me.docx")
-print(my_document)
+#Find a way to convert docx and pdf into txt files
+#Easier for reading and parsing.
+import string
+#open File
+with open("french.txt") as fh:
+    count = 0
+    for line in fh:
+        if line != '':
+            print(str(count)+ " " + line)
+            count += 1
+        #Split Paragraph on basis of '.' or ? or !.
+
+        for l in re.split(r"\-",line):
+            #Split line into list using space.
+            print(  l )
